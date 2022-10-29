752f94248b3c78d944a7cfbb4802afca6a824b15
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-10-29 22:01:34 +0700

    feat: create wallet and transaction admin pages

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from wallet.models import Transaction, Wallet
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    pass
+
+
+class TransactionInline(admin.StackedInline):
+    model = Transaction
+    extra = 1
+
+
 # Register your models here.
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    inlines = [TransactionInline]
