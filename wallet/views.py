3a8711c8ef30efc22077bf249b7874adc771039c
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-10-29 23:07:17 +0700

    refactor: move templates

--- wallet/views.py
+++ wallet/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 # Create your views here.
 @login_required(login_url='/authentication/login')
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, 'index.html')
+    return render(request, 'wallet/index.html')
