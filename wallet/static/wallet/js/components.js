032daf38cb54a1c38669373fb65e091c2d7be518
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-10-30 11:47:26 +0700

    feat: add link to wallet detail

--- wallet/static/wallet/js/components.js
+++ wallet/static/wallet/js/components.js
@@ -1,6 +1,8 @@
 const walletCard = (wallet) => `
   <div class="wallet__wallet">
-    <p class="wallet__wallet-name">${wallet.name}</p>
+    <p class="wallet__wallet-name">
+      <a href="/wallet/${wallet.id}">${wallet.name}</a>
+    </p>
     <p class="wallet__wallet-text">Rp. ${wallet.balance}</p>
   </div>
 `
