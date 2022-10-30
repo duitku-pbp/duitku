ee251300f5f2e747cdb73d965d24cff6e5c0d0e1
Nayyara Airlangga <nayyara.airlangga@gmail.com>
2022-10-30 12:45:54 +0700

    feat: add loading spinner

--- wallet/static/wallet/js/index.js
+++ wallet/static/wallet/js/index.js
@@ -1,6 +1,17 @@
 // Removes base title
 document.getElementsByTagName('title')[0].remove()
 
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    document.getElementById("wallet__loading").hidden = true
+    document.getElementById("wallet__container").hidden = false
+  } else {
+    document.getElementById("wallet__loading").hidden = false
+    document.getElementById("wallet__container").hidden = true
+  }
+}
+
+
 document.addEventListener('DOMContentLoaded', async () => {
   const csrftoken = getCookie('csrftoken')
   const route = window.location.pathname
