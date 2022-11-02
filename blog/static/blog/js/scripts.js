634c268f154e966f35673513da00f367e7eb8dcc
Andrew Jeremy <Andrewjeremy12345@gmail.com>
2022-11-02 17:44:43 +0700

    refactor: add ajax to get all blog posts

---
+++ blog/static/blog/js/scripts.js
@@ -0,0 +1,24 @@
+const blog_entry = (post) => `<li style="list-style: none">
+			<a href="/blog/detail/${post.id}/">
+				<div class="content" style="position: relative">
+					<div id="upvote" style="position: absolute; bottom: 20px; right: 20px">
+						<div id="upvote-counter">${post.upvotes}</div>
+						<img src="/static/blog/images/heart.png" id="heart" />
+					</div>
+					<h1>${post.title}</h1>
+					<ul>
+						<li>${post.first_sentence}</li>
+						<li id="time">Posted on the ${post.pub_date}.</li>
+						<li>${post.get_comment_amount} comments</li>
+					</ul>
+				</div>
+			</a>
+		</li>`;
+
+$(document).ready(function () {
+	$.get("/blog/get_posts/10", function (data) {
+		data.data.map((post) => {
+			$(".post-container").append(blog_entry(post));
+		});
+	});
+});
